62dcb6b1b97a6876df05cac8b40b00b8aad1fae5
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-12-13 02:10:27 +0530

    Add files via upload

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            mid = i + (j-i)//2
+            if nums[mid] == target :
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+        if j == mid:
+            return mid +1 
+        else:
+            return mid
