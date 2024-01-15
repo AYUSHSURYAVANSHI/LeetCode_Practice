acfe02ebe77b1abaad762f1a156e33f07cfd7000
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-01-15 19:40:53 +0530

    Add files via upload

--- 1657. Determine if Two Strings Are Close.py
+++ 1657. Determine if Two Strings Are Close.py
@@ -3,5 +3,6 @@ class Solution:
         f1 = Counter(word1)
         f2 = Counter(word2)
         if f1.keys() != f2.keys():
+            
             return False
         return sorted(list(f1.values())) == sorted(list(f2.values()))
