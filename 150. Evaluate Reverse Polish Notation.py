894daa35ea50746bcc44581007ee2e08a0e625b4
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-01-30 11:49:34 +0530

    Add files via upload

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,25 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Initialize an empty stack to store operands
+        stack = []
+        
+        # Iterate through each token in the input list
+        for n in tokens:
+            # If the token is an operator, perform the corresponding operation
+            if n == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif n == '-':
+                x, y = stack.pop(), stack.pop()
+                stack.append(y - x)
+            elif n == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif n == '/':
+                x, y = stack.pop(), stack.pop()
+                # Perform integer division and append the result to the stack
+                stack.append(int(y / x))
+            else:
+                # If the token is an operand, convert it to an integer and append to the stack
+                stack.append(int(n))
+        
+        # The final result is the only element left in the stack
+        return stack[-1]
