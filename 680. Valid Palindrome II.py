f182e17615d52c53c5161cc50c03c9dc406984bb
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-02-23 23:39:56 +0530

    Add files via upload

---
+++ 680. Valid Palindrome II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        i,j=0, len(s)-1
+        while i<j:
+            if s[i] != s[j]:
+                return s[:i]+s[i+1:] == (s[:i]+s[i+1:])[::-1] or s[:j]+s[j+1:] == (s[:j]+s[j+1:])[::-1]
+            i+=1
+            j-=1
+        return True
