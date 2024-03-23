46c241258eb2d7720701921774b542f5f1ee6c89
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-03-23 23:33:13 +0530

    Add files via upload

---
+++ 143. Reorder List.py
@@ -0,0 +1,34 @@
+
+class Solution:
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+
+        # Step 1: Find the middle of the list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the list
+        prev, curr = None, slow
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+
+        # Step 3: Merge the two halves
+        first, second = head, prev
+        while second.next:
+            # Save next pointers
+            tmp1 = first.next
+            tmp2 = second.next
+            
+            # Reorder nodes
+            first.next = second
+            second.next = tmp1
+            
+            # Move pointers forward
+            first = tmp1
+            second = tmp2
