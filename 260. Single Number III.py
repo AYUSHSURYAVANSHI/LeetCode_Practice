8fed16f6542ad457b44bd1a9be54712ec0a85151
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-05-31 23:17:37 +0530

    Add files via upload

--- 260. Single Number III.py
+++ 260. Single Number III.py
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        nums = Counter(nums)
-        l = []
-        for n, o in nums.items():
-            if o == 1:
-                l.append(n)
-        return l
-=======
-class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
-        nums = Counter(nums)
-        l = []
-        for n, o in nums.items():
-            if o == 1:
-                l.append(n)
-        return l
->>>>>>> 1d9420b076e7346079e9d42c8776d2fbfb052808
+    def singleNumber(self, nums: list[int]) -> list[int]:
+        n: int = len(nums)
+        result: list[int] = [0, 0]
+        index = 0
+
+        for i in range(n):
+            found = False
+            for j in range(n):
+                if i != j and nums[i] == nums[j]:
+                    found = True
+                    break
+            if not found:
+                result[index] = nums[i]
+                index += 1
+                if index == 2:
+                    break
+
+        return result
