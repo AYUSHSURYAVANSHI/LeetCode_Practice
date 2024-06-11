d6176b8f03827d560ce295bea50e243a1a426d99
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-06-11 10:55:28 +0530

    Add files via upload

--- 1122. Relative Sort Array.py
+++ 1122. Relative Sort Array.py
@@ -1,9 +1,12 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        sorted_arr = []
-
-        for x in arr2:
-            while x in arr1:
-                sorted_arr.append(x)
-                arr1.remove(x)
-        return (sorted_arr + sorted(arr1))
+        a = 0
+        # Step 1: Relative Ordering
+        for i in range(len(arr2)):
+            for j in range(len(arr1)):
+                if arr1[j] == arr2[i]:
+                    arr1[a], arr1[j] = arr1[j], arr1[a]
+                    a += 1
+        # Step 2: Sorting Remaining Elements
+        arr1[a:] = sorted(arr1[a:])
+        return arr1
