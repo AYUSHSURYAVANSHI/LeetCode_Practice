e55a977bc63de37399bbf2fa0bb62a8bb9319d48
AYUSHSURYAVANSHI <surywanshiayush@gmail.com>
2024-07-03 21:39:23 +0530

    July

--- 2331. Evaluate Boolean Binary Tree.py
+++ 2331. Evaluate Boolean Binary Tree.py
@@ -3,7 +3,7 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
+#         self.right = right    
 class Solution:
     def helper(self, root):
         if root.val == 0 or root.val == 1:
