b157fea9bf1723288e6ea87b85760dd1e76bc3fb
AYUSHSURYAVANSHI <surywanshiayush@gmail.com>
2024-08-07 14:01:41 +0530

    Upload

--- 273. Integer to English Words.py
+++ 273. Integer to English Words.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution:
     def numberToWords(self, num: int) -> str:
         if num == 0:
@@ -34,41 +33,3 @@ class Solution:
             if num > 0:
                 result += digitString[num] + " "
         
-=======
-class Solution:
-    def numberToWords(self, num: int) -> str:
-        if num == 0:
-            return "Zero"
-        
-        bigString = ["Thousand", "Million", "Billion"]
-        result = self.numberToWordsHelper(num % 1000)
-        num //= 1000
-        
-        for i in range(len(bigString)):
-            if num > 0 and num % 1000 > 0:
-                result = self.numberToWordsHelper(num % 1000) + bigString[i] + " " + result
-            num //= 1000
-        
-        return result.strip()
-
-    def numberToWordsHelper(self, num: int) -> str:
-        digitString = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-        teenString = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-        tenString = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-        
-        result = ""
-        if num > 99:
-            result += digitString[num // 100] + " Hundred "
-        
-        num %= 100
-        if num < 20 and num > 9:
-            result += teenString[num - 10] + " "
-        else:
-            if num >= 20:
-                result += tenString[num // 10] + " "
-            num %= 10
-            if num > 0:
-                result += digitString[num] + " "
-        
->>>>>>> 45936108b93e7559b13be06f63781db262708cea
-        return result
