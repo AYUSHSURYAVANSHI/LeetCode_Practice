1aa3e33cb42c877297a6a930a666f41ad917cb1a
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-09-02 22:02:10 +0530

    Add files via upload

---
+++ 1894. Find the Student that Will Replace the Chalk.py
@@ -0,0 +1,11 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        k, i = k % sum(chalk), 0 # last iterative value of k before negative
+        while k > 0:
+            k -= chalk[i]
+            if k < 0: return i  # return i here, since this is who needs to change the chalk
+            i += 1
+        return i # actually never reaches this step :-), just necessary for syntax
+
+        #Technically 6 line solution haha
+    
