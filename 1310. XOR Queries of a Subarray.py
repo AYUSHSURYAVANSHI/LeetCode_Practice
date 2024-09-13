f0b460ce0977ecc4943895df91f5bf3e80f2bc86
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2024-09-13 15:48:32 +0530

    Add files via upload

---
+++ 1310. XOR Queries of a Subarray.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def xorQueries(self, arr, queries):
+        """
+        :type arr: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        n = len(arr)
+        prefix = [0] * n
+        ans = []
+
+        # Fill the prefix XOR array
+        prefix[0] = arr[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] ^ arr[i]
+
+        # Process each query
+        for left, right in queries:
+            if left == 0:
+                ans.append(prefix[right])
+            else:
+                ans.append(prefix[right] ^ prefix[left - 1])
+
+        return ans
