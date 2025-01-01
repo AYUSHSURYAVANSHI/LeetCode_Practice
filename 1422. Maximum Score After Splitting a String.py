6e79af0cf69a99961d86a1c319f1bdf02042bcdd
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2025-01-01 16:46:02 +0530

    Add files via upload

--- 1422. Maximum Score After Splitting a String.py
+++ 1422. Maximum Score After Splitting a String.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        l = 1
-        score = []
-
-        while 1:
-            
-            if l == len(s):
-                break
-
-            score.append(s[:l].count('0') + s[l:].count('1'))
-
-            l += 1
-        return max(score)
+        total_zeros = s.count('0')
+        ans, zeros, n = -1, 0, len(s)
+        for i in range(1, n):
+            if s[i - 1] == '0':
+                zeros += 1
+                total_zeros -= 1
+            ans = max(ans, zeros + (n - total_zeros - i))
+        return ans
