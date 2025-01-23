faa9c3e5e9967e181dc8e1af7f61572f7a0ac940
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2025-01-24 00:02:22 +0530

    Add files via upload

---
+++ 1267. Count Servers that Communicate.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        Rows = [sum(row) for row in grid]
+        Col = [sum(grid[i][j] for i in range(len(grid))) for j in range(len(grid[0]))]
+        return sum(1 for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == 1 and (Rows[i] > 1 or Col[j] > 1))
