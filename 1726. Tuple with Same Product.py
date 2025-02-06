9cb1b12376c8dbe99efbecedf3f719c71023117e
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2025-02-06 23:47:26 +0530

    Add files via upload

---
+++ 1726. Tuple with Same Product.py
@@ -0,0 +1,13 @@
+class Solution:
+  def tupleSameProduct(self, nums: list[int]) -> int:
+    
+    ans = 0
+    count = collections.Counter()
+
+    for i in range(len(nums)):
+      for j in range(i):
+        prod = nums[i] * nums[j]
+        ans += count[prod] * 8
+        count[prod] += 1
+
+    return ans
