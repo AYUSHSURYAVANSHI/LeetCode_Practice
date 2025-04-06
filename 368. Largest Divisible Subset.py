ad39fa5d20bf7ce1c61bdc39141487fdfb2e87bf
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2025-04-07 01:36:36 +0530

    Add files via upload

--- 368. Largest Divisible Subset.py
+++ 368. Largest Divisible Subset.py
@@ -1,26 +1,24 @@
-<<<<<<< HEAD
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
-        n=len(nums)
-        if n<2:
-            return nums
-        ans=[ [num] for num in nums]   
-        for i in range(n):
+        n = len(nums)
+        dp = [1] * n
+        max_size, max_index = 1, 0
+
+        for i in range(1, n):
             for j in range(i):
-                if nums[i]%nums[j]==0 and len(ans[i])<len(ans[j])+1:
-                    ans[i]=ans[j]+[nums[i]]
-=======
-class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        n=len(nums)
-        if n<2:
-            return nums
-        ans=[ [num] for num in nums]   
-        for i in range(n):
-            for j in range(i):
-                if nums[i]%nums[j]==0 and len(ans[i])<len(ans[j])+1:
-                    ans[i]=ans[j]+[nums[i]]
->>>>>>> 973c2521232a2cdb9a3c2784c3886fa4feeff66d
-        return max(ans,key=lambda x:len(x))  
+                if nums[i] % nums[j] == 0:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    if dp[i] > max_size:
+                        max_size = dp[i]
+                        max_index = i
+
+        result = []
+        num = nums[max_index]
+        for i in range(max_index, -1, -1):
+            if num % nums[i] == 0 and dp[i] == max_size:
+                result.append(nums[i])
+                num = nums[i]
+                max_size -= 1
+
+        return result
